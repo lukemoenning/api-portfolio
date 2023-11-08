3eedf72bccc22f047db81fb373dc1771dcf91f1e
lukemoenning <luke.moenning@gmail.com>
2023-11-07 19:36:14 -0600

    saving to remote

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,71 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from 'express'
 
-const app = express();
-const port: number = 8080;
+const app = express()
+const port: string = process.env.SERVER_PORT || '8080'
+
+app.use(express.json())
+
+const dbo = require('./db/conn')
 
 // define a route handler for the default home page
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello worldjlgjjl!');
-});
+  res.send('Hello worldjlgjjl!')
+})
+
+
+/** ABOUT SECTION */
+app.get('/about', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+
+/** EXPERIENCE SECTION */
+app.get('/experience', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+app.get('/experience/:id', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+
+/** EDUCATION SECTION */
+app.get('/education', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+app.get('/education/:id', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+
+/** SKILLS SECTION */
+app.get('/skills', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+/** PROJECTS SECTION */
+app.get('/projects', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+app.get('/projects/:id', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+
+/** CONTACT SECTION */
+app.get('/contact', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+
 
 // start the Express server
 app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-});
+  dbo.connectToServer(function (error: Error) {
+    if (error) console.error(error)
+  })
+
+  console.log(`server started at http://localhost:${port}`)
+})
